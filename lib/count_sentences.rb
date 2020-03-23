96bad3ccf2a83a1a8f7026cad36502879b7588e7
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-23 21:37:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+     self.end_with?("?")
   end
 
   def exclamation?
-
+     self.end_with?("!")
   end
 
   def count_sentences
+    if self == "" || self == nil
+      0
+    else
+    point = self.split(".").size
+    self.split("!").size - 1 + point
+    end
 
   end
 end
